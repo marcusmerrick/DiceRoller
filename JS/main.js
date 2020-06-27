61de1a9d77c84c182f3d529fd81e5cf6dea4ea0d
Marcus Merrick <marcusmerrick@hotmail.com>
2020-06-26 20:11:59 -0400

    2/3 bonus

--- JS/main.js
+++ JS/main.js
@@ -1,12 +1,43 @@
+//variables
 let dieRolls = []
-const rollDice = document.querySelector('#roll')
+const rollDice = document.querySelector('.roll')
+const numDie = document.querySelector('.numDie')
+const total = document.querySelector('#total')
+const show = document.querySelector('.show')
+const sumResults = document.querySelector('.sumResults')
+const numSides = document.querySelector('.numSides')
+const ok = document.querySelector('#ok')
 
+//functions
+function diceRandom() {
+    return 1 + Math.floor(Math.random()*(numSides.value))
+}
+
+//Dice roll and total
 rollDice.addEventListener('click', function() {
-    let number = document.getElementById('#dice').value
-    
+    let numDieValue = numDie.value   
+    for(let i = 0; i < numDieValue; i++){
+        dieRolls.push(diceRandom())
+    }
+    console.log(dieRolls)
+    let reduceTotal = dieRolls.reduce((a, b) => a + b)
+   console.log(reduceTotal)
+total.innerHTML = reduceTotal
 })
 
-function diceRandom () {
-    return 1 + Math.floor(Math.random()*6)
-}
-console.log(diceRandom)
+//Show All
+show.addEventListener('click', function() {
+    
+    console.log(dieRolls)
+        for(let j = 0; j < dieRolls.length; j++) {
+       console.log(dieRolls[j])
+       sumResults.innerHTML += "<li>" + dieRolls[j] + '</li>'
+       
+    }
+})
+
+//Reset Button
+reset.addEventListener('click', function() {
+    location.reload()
+})
+
